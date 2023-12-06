ae64c82b6afa678e54b3b1b80b7b6a8d6b6b0ce7
laiscosta25 <laiscosta.c2@gmail.com>
2023-12-06 12:47:33 -0300

    Resolvido o problema com o import dos testes

---
+++ test/Files/links.md
@@ -0,0 +1,10 @@
+# 1. Prefácio
+
+[Markdown](https://pt.wikipedia.org/wiki/Markdown) é uma linguagem de marcação
+muito popular entre os programadores. É usada em muitas plataformas que
+manipulam texto (GitHub, fórum, blogs e etc) e é muito comum encontrar arquivos
+com este formato em qualquer repositório (começando pelo tradicional
+`README.md`).
+[Node.js](https://nodejs.org/lksndflksnf), que leia e analise arquivos no formato
+Link de exemplo
+* [Arranjos](https://curriculum.laboratoria.la/pt/topics/javascript/04-arrays)
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,16 +1,26 @@
-const mdLinks = require('../cli.js');
+const { mdLinks } = require('../index.js');
 
-
+console.log(mdLinks)
 describe('mdLinks', () => {
-  const caminhoArquivo = './README.md';
+  const caminhoArquivo = './test/Files/links.md';
   const options = {validate: false};
 
   it('deveria retornar links extraídos sem validação quando o validate for false', () => {
     return mdLinks(caminhoArquivo, options).then((links) => {
       expect(links).toStrictEqual([
-        {href: 'https://pt.wikipedia.org/wiki/Markdown', text: 'Markdown', file: './README.MD'},
-        {href: 'https://nodejs.org/lksndflksnf', text: 'Node.js', file: './README.MD'},
-        {href: 'https://curriculum.laboratoria.la/pt/topics/javascript/04-arrays', text: 'Arranjos', file: './README.MD'},
+        {href: 'https://pt.wikipedia.org/wiki/Markdown', text: 'Markdown', file: './test/Files/links.md'},
+        {href: 'https://nodejs.org/lksndflksnf', text: 'Node.js', file: './test/Files/links.md'},
+        {href: 'https://curriculum.laboratoria.la/pt/topics/javascript/04-arrays', text: 'Arranjos', file: './test/Files/links.md'},
+      ])
+    })
+  });
+
+  it('deveria retornar links extraídos com validação quando o validate for true', () => {
+    return mdLinks(caminhoArquivo, options).then((links) => {
+      expect(links).toStrictEqual([
+        {href: 'https://pt.wikipedia.org/wiki/Markdown', text: 'Markdown', file: './test/Files/links.md'},
+        {href: 'https://nodejs.org/lksndflksnf', text: 'Node.js', file: './test/Files/links.md'},
+        {href: 'https://curriculum.laboratoria.la/pt/topics/javascript/04-arrays', text: 'Arranjos', file: './test/Files/links.md'},
       ])
     })
   });
